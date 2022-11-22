c2fa3058f32b19872717d12cd8ba6f427d2f365c
Peter Plak <peter_plak@dell.com>
2022-11-22 10:47:48 +0100

    update

--- startcicd.py
+++ startcicd.py
@@ -905,7 +905,7 @@ if 'creategns3project' in sys.argv[1:]: #Add nodes to project in GNS3
         print('Then restart.')
        
         #This block is for testing, comment if done testing
-        #"""
+        """
         resp = json.loads(request ( urltuple, "get" )) #Query project to find ID
         #print(resp)
         #print(urltuple)
@@ -913,7 +913,7 @@ if 'creategns3project' in sys.argv[1:]: #Add nodes to project in GNS3
             if obj['name'] == urltuple[3]['name']:
                 print('Project ID : ' + obj['project_id'])
                 response = json.dumps(obj)
-        #"""
+        """
 
         print('proceed = True')
         sys.exit() #Activate when done testing
